cc3b176b92b52315b9446f5ea6ead17169c13108
Alec Rippberger <arippberger@gmail.com>
2014-04-02 18:12:18 -0500

    Updated license and author info

--- dns-lorem-ipsum-generator.php
+++ dns-lorem-ipsum-generator.php
@@ -4,9 +4,24 @@ Plugin Name: DNS Ipsum - Due North Studios Lorem Ipsum Generator
 Plugin URI: https://github.com/arippberger/dns-lorem-ipsum-generator
 Description: Adds lorem ipsum text via the [dns-ipsum] shortcode and a TinyMCE button 
 Version: 1.1
-Author: Due North Studios, LLC
+Author: arippberger, duenorthstudios
 Author URI: http://DueNorthStudios.com
 License: GPL2
+*/
+/*  Copyright 2014  Due North Studios, LLC  (email : alec@duenorthstudios.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 if ( !class_exists( 'dns_ipsum_generator' ) ) {
 	class dns_ipsum_generator {
